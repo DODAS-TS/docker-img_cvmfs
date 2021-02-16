3bf39469eb3fb44d747b8b4ef5e36643c5b23c6e
MircoT <MircoT@users.noreply.github.com>
2021-02-16 16:13:27 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ -z $REPO_LIST ]; then
+if [[ -z $REPO_LIST ]]; then
 
     echo "Env variable REPO_LIST must be specified in the form REPO_LIST=\"cms.cern.ch oasis.cern.ch\""
 
 else
-
     for repo in $REPO_LIST; do
-        if ! [ -d /cvmfs/$repo ]; then
+        if ! [[ -d /cvmfs/$repo ]]; then
             mkdir /cvmfs/$repo
         fi
-        umount /cvmfs/$repo
-        mount -t cvmfs $repo /cvmfs/$repo
+	if mountpoint -q /cvmfs/$repo ; then
+	    umount /cvmfs/$repo
+	fi
+	
+    mount -t cvmfs $repo /cvmfs/$repo
     done
+
     sleep infinity
-
-
 fi
